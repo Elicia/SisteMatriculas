852b4ae0d34b07aa3f5901327fca87fed468b320
Elicia <elicia_cor@hotmail.com>
2013-01-21 10:34:21 -0500

    r1

---
+++ principal/forms.py
@@ -0,0 +1,30 @@
+#encoding:utf-8
+from principal.models import Alumno, Profesor, Curso, Matricula, Dictar, Nota
+from django import forms
+from django.forms import ModelForm
+
+
+
+class AlumnoForm(ModelForm):
+	class Meta:
+		model=Alumno
+
+class CursoForm(ModelForm):
+	class Meta:
+		model=Curso
+
+class ProfesorForm(ModelForm):
+	class Meta:
+		model=Profesor
+
+class ContactoForm(forms.Form):
+	correo = forms.EmailField(label='Tu correo electronico')
+	mensaje = forms.CharField(widget=forms.Textarea)
+
+class MatriculaForm(ModelForm):
+	class Meta:
+		model=Matricula
+
+class EditarAlumno(forms.ModelForm):
+    class Meta:
+        model = Alumno
